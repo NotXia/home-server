ef3f5f37960259fd9087e3bbd668872cd1e956d0
NotXia <35894453+NotXia@users.noreply.github.com>
2024-09-01 21:07:15 +0200

    Add installation script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+prompt_y_n () {
+    read -p "$1 " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo
+        return 0
+    elif [[ $REPLY =~ ^[Nn]$ ]]; then
+        echo
+        return 1
+    else
+        return $2
+    fi
+}
+
+ln_env_and_start_compose () {
+    ln -s ../.env .env
+    docker compose up -d
+    cd ..
+}
+
+
+if prompt_y_n "Start Traefik? [Y/n]" 0; then
+    cd traefik
+    ln_env_and_start_compose
+fi
+
+if prompt_y_n "Start Pi-hole? [Y/n]" 0; then
+    cd pihole
+    ln_env_and_start_compose
+fi
+
+if prompt_y_n "Start website (I mean, you probably don't want this but who am I to judge)? [y/N]" 1; then
+    cd website
+    ln_env_and_start_compose
+fi
