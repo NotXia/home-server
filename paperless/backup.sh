d1896f9d71353f4f71687761e59faa327c7ab7d6
NotXia <35894453+NotXia@users.noreply.github.com>
2024-12-29 20:10:43 +0100

    Fix paperless backup

--- paperless/backup.sh
+++ paperless/backup.sh
@@ -4,9 +4,8 @@
 # $1: restic repository path
 #
 
-mkdir ./export/backup
 docker exec -t paperless document_exporter /usr/src/paperless/export/backup
 
 restic --repo $1 backup --tag paperless ./export/backup
 
-rm -rf ./export/backup
+rm -rf ./export/backup/*
